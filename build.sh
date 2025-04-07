be059ac243288c5a4f68ab21747015e3708325c1
Anthropohedron <Anthropohedron@users.noreply.github.com>
2025-04-07 19:37:50 -0400

    make it easy to use "cargo watch"

--- build.sh
+++ build.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
-exec cargo build \
+WATCH=false
+if test $# -gt 0
+then
+	if test $# -eq 1 -a x"$1" = "x-w"
+	then
+		WATCH=true
+	else
+		echo "Usage: $0 [-w]" >&2
+		exit 1
+	fi
+fi
+
+CONFIG1='target.aarch64-unknown-linux-gnu.linker = "/usr/bin/aarch64-linux-gnu-gcc"'
+set -- build \
 	--target=aarch64-unknown-linux-gnu \
-	--config 'target.aarch64-unknown-linux-gnu.linker = "/usr/bin/aarch64-linux-gnu-gcc"'
+	--config
+
+if $WATCH
+then
+	set -- watch "$@" "'$CONFIG1'"
+else
+	set -- "$@" "$CONFIG1"
+fi
+exec cargo "$@"

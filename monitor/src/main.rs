3c2f994e105810807a859b507a73ee4798146a63
Anthropohedron <Anthropohedron@users.noreply.github.com>
2025-04-10 09:27:48 -0400

    file persistence, add bias, and fix formatting

--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -1,6 +1,7 @@
 extern crate gpiod;
 
-use gpiod::{Chip, Options, Edge, EdgeDetect, Lines, Input};
+use gpiod::{Bias, Chip, Edge, EdgeDetect, Input, Lines, Options};
+use std::env;
 
 mod persist;
 use persist::{DoorStatus, Updater};
@@ -9,9 +10,9 @@ const DOOR_OPEN_PIN: u32 = 12;
 const DOOR_CLOSED_PIN: u32 = 16;
 
 fn init_gpio() -> Lines<Input> {
-    let chip = Chip::new("gpiochip0")
-        .expect("Could not start monitoring GPIO");
+    let chip = Chip::new("gpiochip0").expect("Could not start monitoring GPIO");
     let opts = Options::input([DOOR_OPEN_PIN, DOOR_CLOSED_PIN]) // configure lines offsets
+        .bias(Bias::PullUp)
         .edge(EdgeDetect::Both) // configure edges to detect
         .consumer("garage-door"); // optionally set consumer string
     return chip.request_lines(opts).expect("Could not access GPIO");
@@ -26,24 +27,38 @@ fn status_change(last: &mut Edge, current: Edge) -> bool {
 }
 
 fn main() -> std::io::Result<()> {
-    let mut updater = Updater::new();
+    let args: Vec<String> = env::args().collect();
+    let filename = match args.len() {
+        0 => None,
+        1 => Some(&args[0]),
+        _ => panic!("Too many arguments!"),
+    };
+    let mut updater = Updater::new(filename);
     let mut inputs = init_gpio();
-    let mut last_edge_open = if inputs.get_values([false; 2])?[0] { Edge::Falling } else { Edge::Rising };
-    let mut last_edge_closed: Edge = if inputs.get_values([false; 2])?[1] { Edge::Falling } else { Edge::Rising };
+    let mut last_edge_open = if inputs.get_values([false; 2])?[0] {
+        Edge::Falling
+    } else {
+        Edge::Rising
+    };
+    let mut last_edge_closed: Edge = if inputs.get_values([false; 2])?[1] {
+        Edge::Falling
+    } else {
+        Edge::Rising
+    };
 
     loop {
         let event = inputs.read_event()?;
         let changed: bool = match event.line {
-           0 => status_change(&mut last_edge_open, event.edge),
-           1 => status_change(&mut last_edge_closed, event.edge),
-           _ => false
+            0 => status_change(&mut last_edge_open, event.edge),
+            1 => status_change(&mut last_edge_closed, event.edge),
+            _ => false,
         };
         if changed {
             match (last_edge_open, last_edge_closed) {
                 (Edge::Rising, Edge::Falling) => updater.update(DoorStatus::Closed),
                 (Edge::Falling, Edge::Rising) => updater.update(DoorStatus::Open),
                 (Edge::Falling, Edge::Falling) => updater.update(DoorStatus::Indeterminate),
-                _ => updater.update(DoorStatus::Invalid)
+                _ => updater.update(DoorStatus::Invalid),
             };
         }
     }
--- monitor/src/persist.rs
+++ monitor/src/persist.rs
@@ -1,49 +1,69 @@
 extern crate syslog;
 
-use std::process;
+use std::{collections::HashMap, fs, process, sync::LazyLock};
 use syslog::{Facility, Formatter3164, Logger, LoggerBackend};
 
 const PROGRAM_NAME: &str = "garagemon";
+const DEFAULT_STATUS_FILENAME: &str = "/var/run/garagemon_status";
+static STATUS: LazyLock<HashMap<DoorStatus, &'static str>> = LazyLock::new(|| {
+    HashMap::from([
+        (DoorStatus::Closed, "Closed"),
+        (DoorStatus::Open, "Open"),
+        (DoorStatus::Indeterminate, "Indeterminate"),
+        (DoorStatus::Invalid, "Invalid"),
+    ])
+});
 
+#[derive(Eq, Hash, PartialEq)]
 pub enum DoorStatus {
-    Closed, Open, Indeterminate, Invalid
+    Closed,
+    Open,
+    Indeterminate,
+    Invalid,
 }
 
 pub struct Updater {
+    status_filename: String,
+    status_filename_new: String,
     logger: Syslogger,
 }
 pub type Syslogger = Logger<LoggerBackend, Formatter3164>;
 
 impl Updater {
-    pub fn new() -> Self {
+    pub fn new(status_filename: Option<&String>) -> Self {
         let formatter = Formatter3164 {
             facility: Facility::LOG_USER,
             hostname: None,
             process: PROGRAM_NAME.into(),
             pid: process::id(),
         };
-        let mut logger = syslog::unix(formatter)
-            .expect("Could not set up syslog logging");
-        logger.info("Started {PROGRAM_NAME}").expect("Logging failed");
+        let mut logger = syslog::unix(formatter).expect("Could not set up syslog logging");
+        logger
+            .info("Started {PROGRAM_NAME}")
+            .expect("Logging failed");
+        let default_filename = DEFAULT_STATUS_FILENAME.to_string();
+        let filename = status_filename.unwrap_or(&default_filename).clone();
         Updater {
-            logger: logger
+            status_filename_new: format!("{filename}.new"),
+            status_filename: filename,
+            logger: logger,
         }
     }
-    
+
+    fn write_file(&self, status: &DoorStatus) {
+        let _ = fs::write(&self.status_filename_new, STATUS[status])
+            .and(fs::rename(&self.status_filename_new, &self.status_filename));
+    }
+
     pub fn update(&mut self, status: DoorStatus) {
+        self.write_file(&status);
         let _ = match status {
-            DoorStatus::Closed => {
-                self.logger.info("Door closed")
-            },
-            DoorStatus::Open => {
-                self.logger.info("Door open")
-            },
-            DoorStatus::Indeterminate => {
-                self.logger.info("Door in motion")
-            },
-            DoorStatus::Invalid => {
-                self.logger.info("Confusingly open and closed simultaneously")
-            }
+            DoorStatus::Closed => self.logger.info("Door closed"),
+            DoorStatus::Open => self.logger.info("Door open"),
+            DoorStatus::Indeterminate => self.logger.info("Door in motion"),
+            DoorStatus::Invalid => self
+                .logger
+                .info("Confusingly open and closed simultaneously"),
         };
     }
 }
